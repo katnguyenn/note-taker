7f200577eefd9c45f1b9a61a412cb839857b69d9
Katherine Nguyen <nguyenkat01@yahoo.com>
2020-12-21 16:06:02 -0800

    Created routes for main page and notes page

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = app => {
+
+// Returns index.html file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Returns notes.html file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+};
